6cbd7c214777c5e3b8de7fde847402736a8453b7
Sai Susarla <sai.susarla@gmail.com>
2016-07-21 23:44:09 +0530

    Added command-line options to run.py

--- run.py
+++ run.py
@@ -3,11 +3,22 @@ from pymongo import MongoClient
 import json
 from pprint import pprint
 import smtplib
+from config import *
+from indicdocs import *
+
 app = Flask(__name__)
 app.secret_key = '123'
 client = MongoClient()	
 db = client.samvit
 import string
+
+(cmddir, cmdname) = os.path.split(argv[0])
+setmypath(os.path.abspath(cmddir))
+print "My path is " + mypath()
+
+def usage():
+    print cmdname + " [-r] [-R] [-d] [-o <workdir>] [-l <local_wloads_dir>] <repodir1>[:<reponame>] ..."
+    exit(1)
 
 
 @app.route('/')
@@ -216,5 +227,69 @@ def removeuser():
         username = request.args.get('username')
         db.user.remove({"username": username})
         return redirect('adminhomepage')
-if __name__ == '__main__':
-	app.run(host="0.0.0.0", debug = True)
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "do:l:p:rRh", ["workdir=", "wloaddir="])
+    except getopt.GetoptError:
+        usage()
+
+    reset = False
+    dbreset = False
+    dbgFlag = False
+    myport = PORTNUM
+    localdir = None
+    wdir = workdir()
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+        elif opt in ("-o", "--workdir"):
+	    wdir=arg
+        elif opt in ("-l", "--wloaddir"):
+            localdir = arg
+        elif opt in ("-p", "--port"):
+            myport = int(arg)
+        elif opt in ("-r", "--reset"):
+            reset = True
+        elif opt in ("-R", "--dbreset"):
+            dbreset = True
+        elif opt in ("-d", "--debug"):
+            dbgFlag = True
+    setworkdir(wdir,myport)
+    print cmdname + ": Using " + workdir() + " as working directory."
+    
+    initworkdir(reset)
+
+    initdb(INDICDOC_DBNAME, dbreset)
+
+    for a in args:
+        components = a.split(':')
+        if len(components) > 1:
+            print "Importing " + components[0] + " as " + components[1]
+            addrepo(components[0], components[1])
+        else: 
+            print "Importing " + components[0]
+            addrepo(components[0], "")
+
+    if localdir:
+        setwlocaldir(localdir)
+    if not path.exists(wlocaldir()):
+        setwlocaldir(DATADIR_BOOKS)
+    os.chdir(workdir())
+
+    print "Available on the following URLs:"
+    for line in mycheck_output(["/sbin/ifconfig"]).split("\n"):
+        m = re.match('\s*inet addr:(.*?) .*', line)
+        if m:
+            print "    http://" + m.group(1) + ":" + str(myport) + "/"
+    app.run(
+      host ="0.0.0.0",
+      port = myport,
+      debug = dbgFlag,
+      use_reloader=False
+     )
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+#if __name__ == '__main__':
+#	app.run(host="0.0.0.0", debug = True)
