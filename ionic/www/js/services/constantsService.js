517fbbd36d32787b1aaa72bc86f1ef6f18af7553
Sai Susarla <sai.susarla@gmail.com>
2016-08-30 09:23:22 +0000

    Added samvit_sb Facebook id

--- ionic/www/js/services/constantsService.js
+++ ionic/www/js/services/constantsService.js
@@ -2,15 +2,16 @@
     'use strict';
 	
 	angular.module("starter").
-    constant('constantsService', {
+    var constantsService_app = {
+		url: 'http://app.samskritabharati.in',
+		facebookAppId : '168029083629718',
+		googleId: '263423522351-9je87ajk003c55riacpphn7hk3dlh5hn' };
+        
+    var constantsService_local = {
 		url: 'http://app.samskritabharati.in',
 		facebookAppId : '1611634565814061',
 		googleId: '263423522351-9je87ajk003c55riacpphn7hk3dlh5hn'
         
-	});
-	
-	
-	
-	
-	
+	};
+    constant('constantsService', constantsService_app);
 })();
